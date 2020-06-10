048240e4e2edb29753aa7796378e51c49b1663c6
Antonia Calia-Bogan <acaliabogan@acaliabogan.dev>
2020-06-10 16:49:39 -0400

    Navbar fixed

--- src/web/services/login.rs
+++ src/web/services/login.rs
@@ -1,1 +1,14 @@
+use actix_web::{HttpRequest, HttpResponse};
+use actix_session::Session;
+use actix_web::web::Data;
+use crate::web::app_data::AppData;
 
+/// The service for the login page.
+/// Receives only GET requests.
+pub async fn login_service(
+    req: HttpRequest,
+    app_data: Data<AppData>,
+    session: Session
+) -> HttpResponse {
+    unimplemented!()
+}
