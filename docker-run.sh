e83d6f644e401ee063cbb58452d3e5500ddaa7b0
Nia Calia-Bogan <acaliabogan@acaliabogan.dev>
2020-12-08 06:04:26 -0500

    wait for postgres to come online

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,11 @@
+# Wait for the database to come online
+set -e
+
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "db" -U "$POSTGRES_USER" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
 diesel setup
 diesel migration run
 cargo run --release
