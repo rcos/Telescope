a2ff031136633f835953a5998f731062d98f5f4e
Nia Calia-Bogan <acaliabogan@acaliabogan.dev>
2020-12-08 03:48:16 -0500

    drop embedded migrations

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,6 @@ extern crate serde;
 #[macro_use]
 extern crate diesel;
 
-#[macro_use]
-extern crate diesel_migrations;
-embed_migrations!();
-
 pub mod util;
 
 mod web;
@@ -68,14 +64,6 @@ fn main() -> std::io::Result<()> {
     // Database pool creation and registration of handlebars templates occurs
     // here.
     let app_data = AppData::new();
-
-    // Run embedded database migrations.
-    let conn = app_data
-        .clone_db_conn_pool()
-        .get()
-        .expect("Could not get database connection.");
-    embedded_migrations::run_with_output(&conn, &mut std::io::stdout())
-        .expect("Could not run migrations.");
 
     // from example at https://actix.rs/docs/http2/
     // to generate a self-signed certificate and private key for testing, use
