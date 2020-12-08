2fd5ae7c3525d231876e55f3582316a7be2cdd35
Nia Calia-Bogan <acaliabogan@acaliabogan.dev>
2020-12-07 22:14:21 -0500

    fix email validation bug

--- src/models/emails.rs
+++ src/models/emails.rs
@@ -7,13 +7,8 @@ use crate::{
 use actix_web::web::block;
 use juniper::{FieldError, FieldResult, Value};
 use lettre::EmailAddress;
-use regex::Regex;
 use uuid::Uuid;
 
-lazy_static! {
-    static ref EMAIL_REGEX: Regex =
-        Regex::new(r#"^[[:alpha:]]+@[[:alpha:]]+(\.[[:alpha:]]+)+$"#).unwrap();
-}
 
 /// Field structure must match that in the SQL migration.
 /// (for diesel reasons it seems)
@@ -67,24 +62,21 @@ impl Email {
 }
 
 impl Email {
-    /// Get the email validation regex.
-    pub fn get_email_regex() -> &'static Regex {
-        &*EMAIL_REGEX
-    }
-
     /// Create a new email object. Return none if email does not
     /// match regex.
     pub fn new<T: Into<String>>(user_id: Uuid, email: T) -> Option<Self> {
-        let email = email.into();
-        if Self::get_email_regex().is_match(&email) {
-            Some(Self {
-                user_id,
-                email,
-                is_visible: false,
+        EmailAddress::new(email.into())
+            .map_err(|e| {
+                error!("Email malformed: {}", e);
             })
-        } else {
-            None
-        }
+            .map(|email: EmailAddress| {
+                Self {
+                    user_id,
+                    email: email.to_string(),
+                    is_visible: false,
+                }
+            })
+            .ok()
     }
 
     /// Create a new email object. This will not fail since the syntactic
