2c6e218cc978479b33e71781f545c9959463103d
Frank Matranga <thefrankmatranga@gmail.com>
2020-12-26 02:45:15 -0500

    Enrollment user history

--- api/routers/enrollments.py
+++ api/routers/enrollments.py
@@ -16,6 +16,7 @@ router = APIRouter(
 
 @router.get("/", response_model=List[EnrollmentOut], summary="List all semester enrollments")
 async def list_enrollments(semester_id: Optional[str] = Query(None, example="202101"),
+                           username: Optional[str] = Query(None, example=None),
                            project_id: Optional[int] = Query(
                                None, example=None),
                            is_project_lead: Optional[bool] = Query(
@@ -27,20 +28,19 @@ async def list_enrollments(semester_id: Optional[str] = Query(None, example="202
                            credits_max: Optional[int] = Query(
                                None, example=None),
                            is_for_pay: Optional[bool] = Query(
-                               None, example=None
-),
-        db: Connection = Depends(get_db)):
+                               None, example=None),
+                           db: Connection = Depends(get_db)):
     """
     List all enrollments meeting the specified filters. Each filter is added as an AND operation, so setting `semester_id='202101'`, `is_project_lead=true`, and `project_id=1` will return the project leads for project 1 in the Spring 2021 semester.
 
     Find all enrolled students taking RCOS for credit with `credits_min=1` and find all students taking RCOS for experience with `credits_max=0, is_for_pay=false`.
     """
 
-    return await fetch_enrollments(db, filter_dict(locals(), ["semester_id", "project_id", "is_project_lead", "is_coordinator", "credits_min", "credits_max", "is_for_pay"]))
+    return await fetch_enrollments(db, filter_dict(locals(), ["semester_id", "username", "project_id", "is_project_lead", "is_coordinator", "credits_min", "credits_max", "is_for_pay"]))
 
 
-@router.get("/{username}", response_model=EnrollmentOut, responses={404: {"description": "Not found"}})
-async def get_enrollment(username: str, semester_id: Optional[str] = Query(None), db: Connection = Depends(get_db)):
+@router.get("/{semester_id}/{username}", response_model=EnrollmentOut, responses={404: {"description": "Not found"}})
+async def get_enrollment(username: str, semester_id: str, db: Connection = Depends(get_db)):
     if semester_id is None:
         raise HTTPException(status_code=501)
 
