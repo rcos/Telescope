2a45622349426ebf11cd3542b68616ee8bc3aedb
Frank Matranga <thefrankmatranga@gmail.com>
2020-12-26 20:04:13 -0500

    Full user CRUD

--- api/db/users.py
+++ api/db/users.py
@@ -1,14 +1,17 @@
-from typing import Any, List, Dict
+from typing import Any, List, Dict, Optional
 from asyncpg import Connection
 from pypika import Query, Table, Field
 from pypika.queries import QueryBuilder
+from pypika.terms import Values
 
 users_t = Table("users")
 user_acc_t = Table("user_accounts")
 
 
 async def fetch_users(db: Connection, filter: Dict[str, Any]) -> List[Dict]:
-    query = Query.from_(users_t).select("*").orderby(users_t.username)
+    query = Query.from_(users_t) \
+        .select("*") \
+        .orderby(users_t.username)
 
     for key, value in filter.items():
         if value is None:
@@ -17,10 +20,30 @@ async def fetch_users(db: Connection, filter: Dict[str, Any]) -> List[Dict]:
     return await db.fetch(str(query))
 
 
-async def fetch_user(db: Connection, username: str) -> Dict:
+async def fetch_user(db: Connection, username: str) -> Optional[Dict]:
     query = Query.from_(users_t) \
-        .select("*").where(users_t.username == username)
+        .select("*") \
+        .where(users_t.username == username)
     return await db.fetchrow(str(query))
+
+
+async def upsert_user(db: Connection, username: str, user_dict: Dict[str, Any]) -> Dict:
+    # Does user exist?
+    user = await fetch_user(db, username)
+    if user:
+        query = Query.update(users_t).where(users_t.username == username)
+        for col, value in user_dict.items():
+            query = query.set(users_t[col], value)
+    else:
+        query = Query.into(users_t).columns(
+            "username", *user_dict.keys()).insert(username, *user_dict.values())
+
+    return await db.fetchrow(str(query) + " RETURNING *")
+
+
+async def delete_user(db: Connection, username: str) -> Optional[Dict]:
+    query = Query.from_(users_t).where(users_t.username == username).delete()
+    return await db.fetchrow(str(query) + " RETURNING *")
 
 
 async def fetch_user_accounts(db: Connection, username: str) -> List[Dict]:
--- api/routers/users.py
+++ api/routers/users.py
@@ -1,3 +1,4 @@
+from asyncpg.exceptions import ForeignKeyViolationError
 from api.utils import filter_dict
 from typing import List, Optional
 from asyncpg.connection import Connection
@@ -7,7 +8,7 @@ from fastapi.param_functions import Depends, Query
 
 from api.schemas.users import UserAccount, UserIn, UserOut
 from api.db import get_db
-from api.db.users import fetch_user, fetch_user_accounts, fetch_users
+from api.db.users import fetch_user, fetch_user_accounts, fetch_users, upsert_user, delete_user as delete_user_from_db
 
 router = APIRouter(
     prefix="/users",
@@ -32,14 +33,19 @@ async def get_user(username: str, db: Connection = Depends(get_db)):
     return user
 
 
-@router.put("/{username}", response_model=UserOut, summary="Update a specific user", responses={404: {"description": "Not found"}})
-async def update_user(username: str, updated_user: UserIn, db: Connection = Depends(get_db)):
-    raise HTTPException(status_code=501)
+@router.put("/{username}", response_model=UserOut, summary="Create or update a user")
+async def create_or_update_user(username: str, user: UserIn, db: Connection = Depends(get_db)):
+    updated_user_dict = await upsert_user(db, username, user.dict(exclude_unset=True))
+    return updated_user_dict
 
 
 @router.delete("/{username}", response_model=UserOut, summary="Delete a specific user", responses={404: {"description": "Not found"}})
 async def delete_user(username: str, db: Connection = Depends(get_db)):
-    raise HTTPException(status_code=501)
+    # TODO: Cascade deletions... probably should be done on DB end
+    deleted_user = await delete_user_from_db(db, username)
+    if deleted_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return deleted_user
 
 
 @router.get("/{username}/accounts", response_model=List[UserAccount], summary="Get specific user's accounts", response_description="Get a user's connected social media and Git platform accounts.", responses={404: {"description": "Not found"}})
