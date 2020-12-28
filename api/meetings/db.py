c32636518c4dd230b9b7813225b6cc1d6283b837
Frank Matranga <thefrankmatranga@gmail.com>
2020-12-27 22:28:30 -0500

    Fetch meetings route

---
+++ api/meetings/db.py
@@ -0,0 +1,34 @@
+from api.db import ARRAY_ANY
+from typing import Any, List, Dict, Optional
+from asyncpg import Connection
+from pypika import Query, Table
+from pypika.terms import Parameter
+
+meet_t = Table("meetings")
+
+
+async def fetch_meetings(conn: Connection, filter: Dict[str, Any]) -> List[Dict]:
+    query = Query.from_(meet_t) \
+        .select("*") \
+        .orderby(meet_t.start_date_time)
+
+    for key, value in filter.items():
+        if value is None:
+            continue
+        query = query.where(value == meet_t[key])
+
+    return await conn.fetch(str(query))
+
+
+async def fetch_project(conn: Connection, project_id: str) -> Optional[Dict]:
+    query = Query.from_(proj_t) \
+        .select("*") \
+        .where(proj_t.project_id == project_id)
+    return await conn.fetchrow(str(query))
+
+
+async def delete_project(conn: Connection, project_id: str) -> Optional[Dict]:
+    query = Query.from_(proj_t) \
+        .where(proj_t.project_id == project_id) \
+        .delete()
+    return await conn.fetchrow(str(query) + " RETURNING *")
--- api/meetings/router.py
+++ api/meetings/router.py
@@ -1,7 +1,11 @@
-from typing import Optional
+from api.utils import filter_dict
+from typing import List, Optional
+from asyncpg.connection import Connection
 from fastapi import APIRouter
 from fastapi.exceptions import HTTPException
-from fastapi.param_functions import Query
+from fastapi.param_functions import Depends, Query
+from api.db import get_db
+from . import schemas, db
 
 router = APIRouter(
     prefix="/meetings",
@@ -9,28 +13,34 @@ router = APIRouter(
 )
 
 
-@router.get("/")
-async def list_meetings(semester_id: Optional[str] = Query(None)):
-    raise HTTPException(status_code=501)
+@router.get("/", response_model=List[schemas.MeetingOut])
+async def list_meetings(
+        semester_id: Optional[str] = Query(None),
+        meeting_type: Optional[str] = Query(None),
+        host_username: Optional[str] = Query(None),
+        is_public: Optional[bool] = Query(None),
+        location: Optional[str] = Query(None),
+        conn: Connection = Depends(get_db)):
+    return await db.fetch_meetings(conn, filter_dict(locals(), ["semester_id", "meeting_type", "host_username", "is_public", "location"]))
 
 
 @router.get("/{meeting_id}")
-async def get_meeting(meeting_id: str):
+async def get_meeting(meeting_id: str, conn: Connection = Depends(get_db)):
     raise HTTPException(status_code=501)
 
 
 @router.put("/{meeting_id}")
-async def update_meeting(meeting_id: str):
+async def update_meeting(meeting_id: str, conn: Connection = Depends(get_db)):
     raise HTTPException(status_code=501)
 
 
 @router.delete("/{meeting_id}")
-async def delete_meeting(meeting_id: str):
+async def delete_meeting(meeting_id: str, conn: Connection = Depends(get_db)):
     raise HTTPException(status_code=501)
 
 
 @router.get("/{meeting_id}/attendances")
-async def get_meeting_attendances(meeting_id: str):
+async def get_meeting_attendances(meeting_id: str, conn: Connection = Depends(get_db)):
     raise HTTPException(status_code=501)
 
 
