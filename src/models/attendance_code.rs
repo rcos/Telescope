515e7607daff2edd27246e7e6e9dbc7bca2ba3d4
Nia Calia-Bogan <acaliabogan@acaliabogan.dev>
2021-01-27 15:48:55 -0500

    Attendance Codes Model

---
+++ src/models/attendance_code.rs
@@ -0,0 +1,24 @@
+use uuid::Uuid;
+use chrono::{DateTime, Utc};
+use crate::schema::attendance_codes;
+
+#[derive(Clone, Deserialize, Serialize, Debug, Insertable, Queryable, Associations)]
+#[table_name = "attendance_codes"]
+pub struct AttendanceCode {
+    /// The attendance/event ID.
+    id: Uuid,
+    /// The id of the user who created it.
+    creator_id: Uuid,
+    /// Timestamp that the attendance code was created at.
+    created_at: DateTime<Utc>,
+    /// Timestamp that the attendance code expires at.
+    expires_at: DateTime<Utc>,
+    /// The id of the group that can access this attendance code.
+    target_id: Uuid,
+    /// The code itself. This is None if not used.
+    code: Option<String>,
+    /// Can attendance be taken using the link?
+    enable_link: bool,
+    /// The passphrase to get attendance. This is none if not used.
+    phrase: Option<String>,
+}
--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,16 +1,12 @@
 pub mod users;
-
 pub mod emails;
-
 pub mod confirmations;
-
 pub mod recoveries;
+pub mod password_requirements;
+pub mod markdown;
+pub mod pagination;
+pub mod attendance_code;
+pub mod attendance_record;
 
 #[cfg(test)]
 mod password_requirement_tests;
-
-pub mod password_requirements;
-
-pub mod markdown;
-
-pub mod pagination;
