23145b02ec1d6042879febe419c12cd2b57ca067
Nia Calia-Bogan <acaliabogan@acaliabogan.dev>
2021-02-02 12:14:33 -0500

    remove login and logout services

--- src/web/services/mod.rs
+++ src/web/services/mod.rs
@@ -3,17 +3,12 @@ use actix_web::web::ServiceConfig;
 pub mod blog;
 pub mod confirm;
 pub mod developers;
-pub mod login;
-pub mod logout;
 pub mod p404;
 pub mod profile;
 
 /// Register services to the actix-web server config.
 pub fn register(config: &mut ServiceConfig) {
     config
-        .service(login::login_get)
-        .service(login::login_post)
-        .service(logout::logout_service)
         .service(confirm::confirmations_page)
         .service(confirm::confirm)
         .service(profile::profile)
