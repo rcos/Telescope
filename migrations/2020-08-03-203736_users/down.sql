e9e1ac1ce0f473c1c15b120e401ccef693e86198
Nia Calia-Bogan <acaliabogan@acaliabogan.dev>
2021-02-02 11:46:25 -0500

    remove diesel migrations

--- migrations/2020-08-03-203736_users/down.sql
+++
@@ -1,6 +0,0 @@
--- This file should undo anything in `up.sql`
-
-DROP TABLE IF EXISTS "lost_passwords";
-DROP TABLE IF EXISTS "confirmations";
-DROP TABLE IF EXISTS "emails";
-DROP TABLE IF EXISTS "users";
