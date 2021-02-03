8762ee468fba9c470ac7e475dad2f7ddfce96c94
Nia Calia-Bogan <acaliabogan@acaliabogan.dev>
2021-02-03 18:56:28 -0500

    I guess we are reporting 500s as a success response :(

--- src/web/services/not_found.rs
+++ src/web/services/not_found.rs
@@ -1,9 +1,10 @@
 use crate::error::TelescopeError;
 use actix_web::HttpResponse;
+use actix_web::error::Error as ActixError;
 
 // Use HttpResponse here because never type is not yet stable.
 /// Respond to all requests with page not found.
 /// Used as default service.
 pub async fn not_found() -> Result<HttpResponse, TelescopeError> {
-    Err(TelescopeError::PageNotFound)
+    Err(TelescopeError::ise("testing"))
 }
