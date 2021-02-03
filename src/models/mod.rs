7c5564003073220c2607ae37e154f64e455813e7
Nia Calia-Bogan <acaliabogan@acaliabogan.dev>
2021-02-02 20:41:20 -0500

    Add user models

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,3 +1,4 @@
 //! Rust struct models reflecting each of the tables defined
 //! by the migrations in rcos-data.
 
+pub mod users;
---
+++ src/models/users.rs
@@ -0,0 +1,46 @@
+//! Models pertaining to users and their accounts on the RCOS database.
+
+use chrono::{Date, Utc, DateTime};
+
+#[derive(Serialize, Deserialize, Copy, Clone, Debug)]
+#[serde(rename_all = "snake_case")]
+pub enum UserRole {
+    Student,
+    Faculty,
+    FacultyAdvisor,
+    Alumn,
+    External,
+    ExternalMentor,
+}
+
+#[derive(Copy, Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "lowercase")]
+pub enum UserAccountType {
+    Rpi,
+    Discord,
+    Mattermost,
+    GitHub,
+    GitLab,
+    BitBucket
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct User {
+    username: String,
+    preferred_name: Option<String>,
+    first_name: String,
+    last_name: String,
+    cohort: Option<i32>,
+    role: UserRole,
+    timezone: String,
+    created_at: DateTime<Utc>
+}
+
+#[derive(Clone, Deserialize, Debug, Serialize)]
+pub struct UserAccount {
+    username: String,
+    #[serde(rename = "type")]
+    ty: UserAccountType,
+    account_id: String,
+    created_at: DateTime<Utc>
+}
