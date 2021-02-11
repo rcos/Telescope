ccd73c3d5e82ebf47b9576ffa9cfdd0c968e1d2b
Nia Calia-Bogan <acaliabogan@acaliabogan.dev>
2021-02-10 19:10:44 -0500

    Refactor OAuth2 authentication redirect handler

--- src/web/services/auth/oauth2_providers/mod.rs
+++ src/web/services/auth/oauth2_providers/mod.rs
@@ -11,6 +11,7 @@ use oauth2::{RedirectUrl, CsrfToken, AuthorizationRequest};
 use std::borrow::Cow;
 use crate::web::csrf;
 use actix_web::http::header::LOCATION;
+use url::Url;
 
 pub mod github;
 
@@ -26,6 +27,28 @@ trait Oauth2IdentityProvider {
 
     /// Add the appropriate scopes for the OAuth authentication request.
     fn add_scopes(auth_req: AuthorizationRequest) -> AuthorizationRequest;
+
+    /// Get the redirect URL for the associated client and build an HTTP response to take the user
+    /// there. Saves the CSRF token in the process.
+    fn auth_response(redir_url: RedirectUrl, http_req: &HttpRequest) -> Result<HttpResponse, TelescopeError> {
+        // Get the client configuration and build out the authentication request parameters.
+        let client = Self::get_client();
+        let mut auth_req: AuthorizationRequest = client
+            // Randomly generate a CSRF token.
+            .authorize_url(CsrfToken::new_random)
+            // Add the redirect URL.
+            .set_redirect_url(Cow::Owned(redir_url));
+
+        // Add the scopes defined by this Identity provider and convert the
+        // request into the target URL and assocated CSRF token.
+        let (url, csrf_token) = Self::add_scopes(auth_req).url();
+
+        // Save CSRF token.
+        csrf::save(Self::SERVICE_NAME, http_req, csrf_token)?;
+
+        // Return the URL in an HTTP redirect response.
+        return Ok(HttpResponse::Found().header(LOCATION, url.as_str()).finish());
+    }
 }
 
 
@@ -37,35 +60,24 @@ where T: Oauth2IdentityProvider + 'static {
 
     const SERVICE_NAME: &'static str = <Self as Oauth2IdentityProvider>::SERVICE_NAME;
 
+    // The registration handler is almost identical to the login handler but not much can be
+    // factored out unfortunately.
+
     fn registration_handler(req: HttpRequest) -> LocalBoxFuture<'static, Result<HttpResponse, TelescopeError>> {
         return Box::pin(async move {
-            Err(TelescopeError::NotImplemented)
+            // Get the redirect URL.
+            let redir_url: RedirectUrl = make_redirect_url(&req, Self::registration_redirect_path());
+            // Redirect the user.
+            return Self::auth_response(redir_url, &req);
         });
     }
 
     fn login_handler(req: HttpRequest) -> LocalBoxFuture<'static, Result<HttpResponse, TelescopeError>> {
         return Box::pin(async move {
-            // Get the base redirect URL build into the OAuth2 authentication request.
+            // Get the redirect URL.
             let redir_url: RedirectUrl = make_redirect_url(&req, Self::login_redirect_path());
-
-            // Get the client configuration and build out the authentication request parameters.
-            let client = Self::get_client();
-            let mut auth_req: AuthorizationRequest = client
-                // Randomly generate a CSRF token.
-                .authorize_url(CsrfToken::new_random)
-                // Add the redirect URL.
-                .set_redirect_url(Cow::Owned(redir_url));
-
-            // Add the scopes defined by this Identity provider and convert the
-            // request into the target URL and assocated CSRF token.
-            let (url, csrf_token) = Self::add_scopes(auth_req).url();
-
-            // Save CSRF token.
-            csrf::save(Self::SERVICE_NAME, &req, csrf_token)?;
-
-            // Return an HTTP Response bringing the user to the appropriate
-            // authentication page.
-            return Ok(HttpResponse::Found().header(LOCATION, url.as_str()).finish());
+            // Redirect the user.
+            return Self::auth_response(redir_url, &req);
         });
     }
 
