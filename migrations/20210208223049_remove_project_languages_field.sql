23efe81962b25df4deafce961c49b8d04d511342
Steven vanZyl <rushsteve1@rushsteve1.us>
2021-02-17 14:37:51 -0500

    Remove old DbMate migrations

--- migrations/20210208223049_remove_project_languages_field.sql
+++
@@ -1,7 +0,0 @@
--- migrate:up
-ALTER TABLE projects
-    DROP COLUMN languages CASCADE;
-
--- migrate:down
-ALTER TABLE projects
-    ADD COLUMN languages VARCHAR[] DEFAULT '{}'::VARCHAR[] NOT NULL;
