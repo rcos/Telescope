3b7b0051a58e796d851c15d3ae91e79b378c4211
Steven vanZyl <rushsteve1@rushsteve1.us>
2021-02-18 20:14:12 -0500

    Fix duplicate users to not show both orders

--- migrations/1613612582809_create_duplicate_users_view/up.sql
+++ migrations/1613612582809_create_duplicate_users_view/up.sql
@@ -1,11 +1,12 @@
 CREATE OR REPLACE VIEW duplicate_users
 AS
-  SELECT a.username AS username_a,
-         b.username AS username_b,
+  SELECT DISTINCT
+         LEAST(a.username, b.username) AS username_a,
+         GREATEST(a.username, b.username)  AS username_b,
          a.first_name,
          a.last_name
   FROM   users AS a
-         join users AS b
-           ON a.first_name = b.first_name
-              AND a.last_name = b.last_name
+         JOIN users AS b
+           ON a.first_name ILIKE b.first_name
+              AND a.last_name ILIKE b.last_name
               AND a.username != b.username;
