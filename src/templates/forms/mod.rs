141e3f01ce44a1fbc77eb070bfca95dc49c8fa3e
Nia Calia-Bogan <acaliabogan@acaliabogan.dev>
2021-03-02 18:18:09 -0500

    WIP Forms

--- src/templates/forms/mod.rs
+++ src/templates/forms/mod.rs
@@ -4,21 +4,48 @@
 //! their own traits.
 
 use serde::Serialize;
+use serde::de::DeserializeOwned;
+use serde_json::Map;
 
 pub mod common;
 
-/// Trait for form fields. Each form field type should implement this trait
-/// to define how to do validation.
-pub trait FormField: Serialize {
-    type Input;
 
-    /// Check if a given input value would be valid for this field of the form.
-    fn validate(&self, value: Self::Input) -> Option<String>;
+/// The type attribute of a text field in a form.
+#[derive(Serialize)]
+#[serde(rename_all = "lowercase")]
+enum TextFieldType {
+    Email,
+    Password,
 }
 
+/// Form fields.
+#[derive(Serialize)]
+struct FormField {
+    /// The path from the templates directory to the handlebars file.
+    #[serde(skip)]
+    handlebars_file: &'static str,
+
+    /// The name of this field in the submitted form object.
+    name: &'static str,
+
+    /// The type attribute of this form field.
+    #[serde(rename = "type")]
+    ty: TextFieldType,
+
+    /// The value to pre-fill the form field with.
+    prefill: Option<String>,
+
+    /// The place-holder to put in the form field on on value.
+    placeholder: Option<String>,
+}
+
+/// A form that the user must fill out.
 #[derive(Serialize)]
 pub struct Form {
     /// The handlebars file that should be used to render this form.
     #[serde(skip)]
     handlebars_file: &'static str,
+
+    /// The fields of this form.
+    fields: Vec<FormField>
 }
