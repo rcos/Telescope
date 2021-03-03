3d7e1841b78b88e3b59c8f9c951b4bb83d6b02ef
Nia Calia-Bogan <acaliabogan@acaliabogan.dev>
2021-03-02 21:53:05 -0500

    WIP forms

--- src/templates/forms/common/text_field.rs
+++ src/templates/forms/common/text_field.rs
@@ -2,3 +2,48 @@
 
 /// Path to the
 const TEMPLATE_PATH: &'static str = "forms/common/text_field";
+
+/// The type attribute of a text field in a form.
+#[derive(Serialize)]
+#[serde(rename_all = "lowercase")]
+pub enum TextFieldType {
+    Email,
+    Password,
+}
+
+/// Form fields.
+#[derive(Serialize)]
+pub struct TextField {
+    /// The HTML element id of this form item.
+    id: String,
+
+    /// The label that appears next to this text field.
+    label: String,
+
+    /// The name of this field in the submitted form object.
+    name: &'static str,
+
+    /// The type attribute of this form field.
+    #[serde(rename = "type")]
+    ty: TextFieldType,
+
+    /// The value to pre-fill the form field with.
+    prefill: Option<String>,
+
+    /// The place-holder to put in the form field on on value.
+    placeholder: Option<String>,
+
+    /// If there was an error with this form field, display this error message.
+    error: Option<String>,
+
+    /// If this form field did not error, display this success message.
+    success: Option<String>,
+
+    /// Function to validate an input and return this field.
+    #[serde(skip)]
+    validator: Box<dyn Fn(Option<String>) -> Self>,
+
+    /// Flag for the validator to set to indicate if a form is valid.
+    #[serde(skip)]
+    is_valid: bool,
+}
--- src/templates/forms/mod.rs
+++ src/templates/forms/mod.rs
@@ -6,46 +6,46 @@
 use serde::Serialize;
 use serde::de::DeserializeOwned;
 use serde_json::Map;
+use common::text_field::TextField;
+use std::collections::HashMap;
 
-pub mod common;
+mod common;
 
+/// Path from the template root to the form template file.
+const TEMPLATE_PATH: &'static str = "forms/form";
 
-/// The type attribute of a text field in a form.
+/// A field in a form.
 #[derive(Serialize)]
-#[serde(rename_all = "lowercase")]
-enum TextFieldType {
-    Email,
-    Password,
+#[serde(rename_all = "snake_case")]
+enum FormField {
+    /// A text field in a form.
+    TextField(TextField)
 }
 
-/// Form fields.
-#[derive(Serialize)]
-struct FormField {
-    /// The path from the templates directory to the handlebars file.
-    #[serde(skip)]
-    handlebars_file: &'static str,
-
-    /// The name of this field in the submitted form object.
-    name: &'static str,
-
-    /// The type attribute of this form field.
-    #[serde(rename = "type")]
-    ty: TextFieldType,
-
-    /// The value to pre-fill the form field with.
-    prefill: Option<String>,
-
-    /// The place-holder to put in the form field on on value.
-    placeholder: Option<String>,
-}
-
-/// A form that the user must fill out.
+/// A form that the user must fill out. All forms submit by `POST` to
+/// the URL they are served at.
 #[derive(Serialize)]
 pub struct Form {
-    /// The handlebars file that should be used to render this form.
-    #[serde(skip)]
-    handlebars_file: &'static str,
+    /// The title that will appear above the form.
+    pub form_title: String,
+    /// The fields of this form. Keys are `name` attributes.
+    fields: HashMap<String, FormField>,
+    /// The text on the submit button.
+    pub submit_button_text: String,
+    /// Any css classes to add to the submit button.
+    /// When `None`, the default is `btn-primary`)
+    /// All submit buttons have `btn` and `btn-spinner` already.
+    pub submit_button_class: Option<String>,
+}
 
-    /// The fields of this form.
-    fields: Vec<FormField>
+impl Form {
+    /// Create a new empty form.
+    fn empty(title: impl Into<String>) -> Self {
+        Self {
+            form_title: title.into(),
+            fields: HashMap::new(),
+            submit_button_text: "Submit".into(),
+            submit_button_class: None
+        }
+    }
 }
