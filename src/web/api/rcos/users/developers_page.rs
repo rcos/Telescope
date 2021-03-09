bf29ee730c347ece0ffa71c1837e02b233d6f77d
Nia Calia-Bogan <acaliabogan@acaliabogan.dev>
2021-03-09 16:03:51 -0500

    Developers page WIP

--- src/web/api/rcos/users/developers_page.rs
+++ src/web/api/rcos/users/developers_page.rs
@@ -9,7 +9,8 @@ use super::UserRole as user_role;
 #[derive(GraphQLQuery)]
 #[graphql(
     schema_path = "graphql/schema.json",
-    query_path = "graphql/users/developers.graphql"
+    query_path = "graphql/users/developers.graphql",
+    response_derives = "Debug"
 )]
 pub struct Developers;
 
@@ -50,6 +51,35 @@ impl Developers {
                 .unwrap_or("%".into()),
             // In practice, we usually only have one order-by parameter.
             order_by: vec![order_by]
+        }
+    }
+}
+
+impl Default for users_order_by {
+    fn default() -> Self {
+        users_order_by {
+            bonus_attendances_aggregate: None,
+            cohort: None,
+            created_at: None,
+            first_name: None,
+            last_name: None,
+            username: None,
+            enrollments_aggregate: None,
+            final_grade_appeals_aggregate: None,
+            meeting_attendances_aggregate: None,
+            meetings_aggregate: None,
+            preferred_name: None,
+            project_pitches_aggregate: None,
+            project_pitches_by_reviewer_username_aggregate: None,
+            project_presentation_grades_aggregate: None,
+            role: None,
+            small_group_mentors_aggregate: None,
+            status_update_submissions_aggregate: None,
+            status_update_submissions_by_grader_username_aggregate: None,
+            timezone: None,
+            user_accounts_aggregate: None,
+            workshop_proposals_aggregate: None,
+            workshop_proposals_by_reviewer_username_aggregate: None
         }
     }
 }
--- src/web/services/developers.rs
+++ src/web/services/developers.rs
@@ -4,9 +4,58 @@ use crate::templates::Template;
 use crate::error::TelescopeError;
 use actix_web::web::Query;
 use crate::web::services::auth::identity::Identity;
+use crate::web::api::rcos::{
+    make_api_client,
+    send_query,
+    users::developers_page::{
+        Developers,
+        developers::{
+            users_order_by,
+            order_by
+        },
+    }
+};
 
 /// The default value for the number of users per page.
-fn twenty() -> u64 { 20u64 }
+fn twenty() -> u32 { 20 }
+
+/// Which field should users be ordered by.
+#[derive(Copy, Clone, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+enum OrderByField {
+    FirstName,
+    LastName,
+}
+
+impl Default for OrderByField {
+    fn default() -> Self {
+        OrderByField::FirstName
+    }
+}
+
+/// What order to use.
+#[derive(Copy, Clone, Debug, Serialize, Deserialize)]
+enum OrderBy {
+    #[serde(rename = "asc")]
+    Ascending,
+    #[serde(rename = "desc")]
+    Descending
+}
+
+impl Default for OrderBy {
+    fn default() -> Self {
+        OrderBy::Ascending
+    }
+}
+
+impl Into<order_by> for OrderBy {
+    fn into(self) -> order_by {
+        match self {
+            OrderBy::Ascending => order_by::asc,
+            OrderBy::Descending => order_by::desc,
+        }
+    }
+}
 
 /// The query parameters passed to the developers page indicating pagination
 /// data and any filters.
@@ -15,12 +64,12 @@ pub struct DevelopersPageQuery {
     /// What page number to go to. Default to 0.
     #[serde(rename = "p")]
     #[serde(default)]
-    pub page: u64,
+    pub page: u32,
 
     /// How many per page. Default to 20.
-    #[serde(rename = "l")]
+    #[serde(rename = "n")]
     #[serde(default = "twenty")]
-    pub per_page: u64,
+    pub per_page: u32,
 
     /// Filter for users if their first name, last name, or username contains
     /// this string case independently (via ILIKE).
@@ -28,16 +77,18 @@ pub struct DevelopersPageQuery {
     pub search: Option<String>,
 
     /// Filter the semester in which the user was enrolled. This should be a semester ID.
-    #[serde(rename = "s")]
+    #[serde(rename = "semester")]
     pub filter_semester: Option<String>,
 
     /// Order the developers by a given field.
-    #[serde(rename = "b")]
-    pub order_by: Option<String>,
+    #[serde(rename = "order_by")]
+    #[serde(default)]
+    order_by: OrderByField,
 
     /// Ascending or descending order.
-    #[serde(rename = "o")]
-    pub order: Option<String>
+    #[serde(rename = "order")]
+    #[serde(default)]
+    order: OrderBy
 }
 
 impl Default for DevelopersPageQuery {
@@ -47,8 +98,25 @@ impl Default for DevelopersPageQuery {
             per_page: twenty(),
             search: None,
             filter_semester: None,
-            order_by: None,
-            order: None
+            order_by: OrderByField::FirstName,
+            order: OrderBy::Ascending
+        }
+    }
+}
+
+impl DevelopersPageQuery {
+    /// Convert this structure's order parameters to an order_by query.
+    pub fn order_by_param(&self) -> users_order_by {
+        match self.order_by {
+            OrderByField::FirstName => users_order_by {
+                first_name: Some(self.order.into()),
+                .. users_order_by::default()
+            },
+
+            OrderByField::LastName => users_order_by {
+                last_name: Some(self.order.into()),
+                .. users_order_by::default()
+            }
         }
     }
 }
@@ -56,6 +124,12 @@ impl Default for DevelopersPageQuery {
 /// The developer catalogue. This page displays all of the users in the
 /// RCOS database.
 #[get("/developers")]
-pub async fn developers_page(identity: Identity, Query(query): Query<DevelopersPageQuery>) -> Result<Template, TelescopeError> {
+pub async fn developers_page(Query(query): Query<DevelopersPageQuery>) -> Result<Template, TelescopeError> {
+    // Extract the number of users to retrieve.
+    let limit: u32 = query.per_page;
+    // Extract the offset into the user data for the API query.
+    let offset: u32 = query.per_page * query.page;
+    // Extract the remaining fields
+
     Err(TelescopeError::NotImplemented)
 }
