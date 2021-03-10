addd1161397d771a216278a14b39ba185ef8fb59
Nia Calia-Bogan <acaliabogan@acaliabogan.dev>
2021-03-09 20:25:33 -0500

    Migrate user_accounts table to use user_ids.

---
+++ migrations/1615338200877_alter_user_accounts_users_foreign_key/up.sql
@@ -0,0 +1,18 @@
+-- This migration updates the foreign key constraint of the user_accounts table
+-- to use the id column of the users table.
+-- This does not modify any other tables.
+
+-- Add the user_id column
+ALTER TABLE user_accounts ADD COLUMN user_id UUID;
+
+-- Set all user ids in the accounts table to the appropriate value.
+UPDATE user_accounts
+SET user_id = id
+FROM users
+WHERE user_accounts.username = users.username;
+
+-- Constrain user_account's user_ids to not be null.
+ALTER TABLE user_accounts ALTER COLUMN user_id SET NOT NULL;
+
+-- Constrain user_account's user_ids to reference a user's id.
+ALTER TABLE user_accounts ADD FOREIGN KEY (user_id) REFERENCES users(id);
