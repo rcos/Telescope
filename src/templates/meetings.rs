c34845748c367a4ba57e2fe46122bf61bc89c78b
Nia Calia-Bogan <acaliabogan@acaliabogan.dev>
2021-03-24 23:26:21 -0400

    Fix meetings template and service

--- src/templates/meetings.rs
+++ src/templates/meetings.rs
@@ -1,12 +1,16 @@
 //! Meetings page template. This is mostly a static template.
 
-use crate::templates::{
-    Template,
-    page
-};
-use crate::error::TelescopeError;
-use actix_web::HttpRequest;
+use crate::templates::Template;
+use crate::web::api::rcos::meetings::get::meetings::MeetingsMeetings;
 
 /// The path to the template's handlebars file.
 const TEMPLATE_NAME: &'static str = "meetings_page";
 
+/// The handlebars key for the list of meetings from the RCOS API response.
+pub const MEETINGS: &'static str = "meetings";
+
+/// Make a meetings page template.
+pub fn make(events: Vec<MeetingsMeetings>) -> Template {
+    Template::new(TEMPLATE_NAME)
+        .field(MEETINGS, events)
+}
--- src/web/services/meetings.rs
+++ src/web/services/meetings.rs
@@ -7,7 +7,7 @@ use crate::templates::{
 use crate::error::TelescopeError;
 use actix_web::web::{ServiceConfig, Json, Query};
 use actix_web::HttpRequest;
-use chrono::{DateTime, Utc, TimeZone, NaiveDateTime};
+use chrono::{DateTime, Utc, TimeZone, NaiveDateTime, Local, Duration};
 use chrono_tz::Tz;
 use crate::web::services::auth::identity::Identity;
 use crate::web::api::rcos::meetings::get::{
@@ -20,76 +20,56 @@ use crate::web::api::rcos::meetings::get::{
 /// Register calendar related services.
 pub fn register(config: &mut ServiceConfig) {
     config
-        .service(calendar_page)
-        .service(events);
+        .service(calendar_page);
+}
+
+/// Event endpoint query parameters used by FullCalendar.
+#[derive(Deserialize, Debug, Copy, Clone)]
+pub struct MeetingsQuery {
+    /// The start time to get events from.
+    pub start: Option<NaiveDateTime>,
+    /// The end time to get events from.
+    pub end: Option<NaiveDateTime>,
 }
 
 /// Calendar page
 #[get("/meetings")]
-async fn calendar_page(req: HttpRequest) -> Result<Template, TelescopeError> {
-    // Render calendar page.
-    meetings::calendar_page(&req).await
-}
+async fn calendar_page(req: HttpRequest, params: Option<Query<MeetingsQuery>>, identity: Identity) -> Result<Template, TelescopeError> {
+    // Resolve parameters to API query variables
+    let start: DateTime<Utc> = params.as_ref()
+        // Extract the start parameter from the query
+        .and_then(|p| p.start.as_ref())
+        // Convert to a timestamp in the local timezone
+        .map(|naive: &NaiveDateTime| Local.from_local_datetime(naive))
+        // If it's ambiguous what timestamp to use in the local timezone, pick the earlier one.
+        .and_then(|local_result| local_result.earliest())
+        // If there is no valid timezone or the start parameter wasn't supplied,
+        // use the current time minus 2 hours. This should be sufficient to catch all
+        // recent and ongoing meetings.
+        .unwrap_or(Local::now() - Duration::hours(2))
+        // Convert timezone to UTC.
+        .with_timezone(&Utc);
 
-/// Event endpoint query parameters used by FullCalendar.
-#[derive(Serialize, Deserialize, Debug, Copy, Clone)]
-pub struct EventsQuery {
-    /// The start time to get events from.
-    pub start: NaiveDateTime,
-    /// The end time to get events from.
-    pub end: NaiveDateTime,
-}
+    let end: DateTime<Utc> = params.as_ref()
+        // Extract the end parameter from the query
+        .and_then(|p| p.end.as_ref())
+        // Convert to a timestamp in the local timezone.
+        .map(|naive: &NaiveDateTime| Local.from_local_datetime(naive))
+        // If the time in the local timezone is ambiguous, use the later one
+        .and_then(|local_result| local_result.latest())
+        // If there is no valid time, or the parameter wasn't supplied,
+        // default to one week from today. This will show all the next meetings.
+        .unwrap_or(Local::now() + Duration::weeks(1))
+        // Convert timezone to UTC.
+        .with_timezone(&Utc);
 
-/// Serializable event type used with FullCalendar.
-/// See https://fullcalendar.io/docs/event-object.
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct FullCalendarEvent {
-    pub id: i64,
-    pub title: String,
-    pub start: DateTime<Utc>,
-    pub end: DateTime<Utc>,
-    pub url: Option<String>,
-    /// The source object from the API.
-    pub meeting: MeetingsMeetings
-}
-
-/// Events endpoint. This should return a JSON list of FullCalendarEvents.
-#[get("/calendar/events")]
-async fn events(identity: Identity, Query(params): Query<EventsQuery>) -> Result<Json<Vec<FullCalendarEvent>>, TelescopeError> {
-    // Check if the user is authenticated -- can we show them events that aren't public.
+    // Is there an RCOS user authenticated?
     let is_authenticated: bool = identity.get_rcos_username().await?.is_some();
+    // The visibility of private events is true if there's an authenticated user.
+    let public_only: bool = !is_authenticated;
 
-    // Convert timezones.
-    let start_utc: DateTime<Utc> = params.time_zone
-        // Get a timestamp with timezone
-        .from_local_datetime(&params.start)
-        // Unwrap the earliest valid time if there is one.
-        .earliest()
-        // Report an error if there is not.
-        .ok_or(TelescopeError::BadRequest {
-            header: "Invalid Query".into(),
-            message: "Invalid start timestamp in request query.".into(),
-            show_status_code: false,
-        })?
-        // Convert to UTC
-        .with_timezone(&Utc);
-
-    // Similar with end time.
-    let end_utc: DateTime<Utc> = params.time_zone
-        .from_local_datetime(&params.end)
-        // Get the latest valid time just ot be safe here.
-        .latest()
-        .ok_or(TelescopeError::BadRequest {
-            header: "Invalid Query".into(),
-            message: "Invalid end time in request query.".into(),
-            show_status_code: false,
-        })?
-        // Convert to UTC.
-        .with_timezone(&Utc);
-
-    // Return the meetings from the API.
-    return Meetings::get(start_utc, end_utc, !is_authenticated)
-        .await
-        // Mapped into json.
-        .map(|meetings| Json(meetings));
+    // Query the RCOS API to get meeting data.
+    let events: Vec<MeetingsMeetings> = Meetings::get(start, end, public_only).await?;
+    // Build a meetings page template, render it into a page for the user.
+    return meetings::make(events).render_into_page(&req, "RCOS Meetings").await;
 }
