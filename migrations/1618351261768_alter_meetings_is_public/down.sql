66e74a57139a5f140259089b3b8f6177d465d0b2
Nia Calia-Bogan <acaliabogan@acaliabogan.dev>
2021-04-13 18:18:01 -0400

    Handle public_meetings view

--- migrations/1618351261768_alter_meetings_is_public/down.sql
+++ migrations/1618351261768_alter_meetings_is_public/down.sql
@@ -8,3 +8,15 @@ UPDATE meetings SET is_public = TRUE WHERE is_draft = FALSE;
 
 -- Remove the is_draft flag.
 ALTER TABLE meetings DROP COLUMN is_draft;
+
+-- Re-create the public meetings view.
+CREATE VIEW public_meetings AS
+SELECT * FROM meetings AS m
+WHERE m.is_public = TRUE
+ORDER BY m.start_date_time;
+
+COMMENT ON VIEW public_meetings IS 'View for access to public meetings';
+
+-- Re-create original comment on is_public column.
+COMMENT ON COLUMN meetings.is_public
+IS 'True if it appears on the schedule publicly (can be used for drafts)';
--- migrations/1618351261768_alter_meetings_is_public/up.sql
+++ migrations/1618351261768_alter_meetings_is_public/up.sql
@@ -9,5 +9,9 @@ IS 'Flag to indicate this meeting is a draft, and the details are not final.';
 -- Set the value to true if the old flag was false.
 UPDATE meetings SET is_draft = TRUE WHERE is_public = FALSE;
 
+-- Drop the public_meetings view. We could update it, but it's preferable for
+-- clients to just use GraphQl filters and ordering in their queries.
+DROP VIEW public_meetings;
+
 -- Remove the old flag.
 ALTER TABLE meetings DROP COLUMN is_public;
