fad580261bd072e600118691b24a9ab8b78a3431
Nia Calia-Bogan <acaliabogan@acaliabogan.dev>
2021-04-30 13:01:41 -0400

    WIP developers page

--- src/templates/navbar.rs
+++ src/templates/navbar.rs
@@ -49,8 +49,8 @@ fn item(req_path: &str, text: impl Into<String>, location: impl Into<String>) ->
 fn with_defaults(req_path: &str) -> Template {
     let left_items = vec![
         item(req_path, "Home", "/"),
-        // item(req_path, "Projects", "/projects"),
-        // item(req_path, "Developers", "/developers"),
+        item(req_path, "Projects", "/projects"),
+        item(req_path, "Developers", "/developers"),
         item(req_path, "Sponsors", "/sponsors"),
         // item(req_path, "Blog", "/blog"),
         item(req_path, "Meetings", "/meetings"),
