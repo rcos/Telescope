4e4ae3d66c244399bf908211fd91c4b389b35830
Nia Calia-Bogan <acaliabogan@acaliabogan.dev>
2021-04-30 20:57:35 -0400

    WIP developers page

--- src/api/rcos/users/developers_page.rs
+++ src/api/rcos/users/developers_page.rs
@@ -1,7 +1,12 @@
 //! RCOS API query to get list of developers to display on the developers page.
 
-// Aliases necessary for generated module's namespace.
-use super::UserAccountType as user_account;
+use crate::api::rcos::prelude::*;
+use regex::Regex;
+use std::borrow::Cow;
+use chrono::Utc;
+
+/// The query returns 20 developers per page.
+const PER_PAGE: u32 = 20;
 
 /// Type representing GraphQL query to get a list of users and their
 /// account associations.
@@ -13,12 +18,7 @@ use super::UserAccountType as user_account;
 )]
 pub struct Developers;
 
-use developers::{
-    users_order_by, DevelopersCurrentSemester, DevelopersUsers, DevelopersUsersUserAccounts,
-    ResponseData, Variables,
-};
-use regex::Regex;
-use std::borrow::Cow;
+use developers::{ResponseData, Variables};
 
 lazy_static! {
     static ref SEARCH_REGEX: Regex = Regex::new(r"[@%\]").unwrap();
@@ -41,162 +41,24 @@ impl Developers {
     /// - `search`: Case insensitive string to match against user's first name,
     ///     last name, or username. This gets escaped before being sent.
     /// - `order_by`: How to order the users requested.
-    pub fn make_variables(
-        limit: u32,
-        offset: u32,
+    fn make_variables(
+        page_num: u32,
         search: Option<String>,
-        order_by: users_order_by,
+        include_old: bool,
     ) -> Variables {
         Variables {
-            limit: limit as i64,
-            offset: offset as i64,
+            limit: PER_PAGE as i64,
+            offset: PER_PAGE*page_num as i64,
             // Search string should default to matching any user.
             search: search
                 // Escape the search string and surround it in `%`s to match on any sequence.
                 .map(|s| format!("%{}%", escape_search_string(s.as_str())))
                 // Default to match any user on no search string.
                 .unwrap_or("%".into()),
-            order_by,
+            include_old,
+            now: Utc::today().naive_utc()
         }
     }
-}
 
-impl Default for users_order_by {
-    fn default() -> Self {
-        users_order_by {
-            bonus_attendances_aggregate: None,
-            cohort: None,
-            created_at: None,
-            first_name: None,
-            last_name: None,
-            username: None,
-            enrollments_aggregate: None,
-            final_grade_appeals_aggregate: None,
-            meeting_attendances_aggregate: None,
-            meetings_aggregate: None,
-            preferred_name: None,
-            project_pitches_aggregate: None,
-            project_pitches_by_reviewer_username_aggregate: None,
-            project_presentation_grades_aggregate: None,
-            role: None,
-            small_group_mentors_aggregate: None,
-            status_update_submissions_aggregate: None,
-            status_update_submissions_by_grader_username_aggregate: None,
-            timezone: None,
-            user_accounts_aggregate: None,
-            workshop_proposals_aggregate: None,
-            workshop_proposals_by_reviewer_username_aggregate: None,
-        }
-    }
-}
-
-/// A user thumbnail on the developers page.
-#[derive(Clone, Deserialize, Serialize, Debug)]
-pub struct DevelopersPageUser {
-    /// The user's username
-    pub username: String,
-    /// The user's first name
-    pub first_name: String,
-    /// The user's last name
-    pub last_name: String,
-    /// The platforms the user has linked.
-    pub user_accounts: Vec<DevelopersUsersUserAccounts>,
-    /// Is the user a coordinator this semester?
-    pub is_current_coordinator: bool,
-    /// The small group this user mentors, if any.
-    pub small_group_id: Option<i64>,
-}
-
-impl DevelopersUsers {
-    /// Resolve the semester data on a user to be current or not.
-    fn resolve(self, current_semester_id: Option<&String>) -> DevelopersPageUser {
-        // Destructure self.
-        let DevelopersUsers {
-            username,
-            first_name,
-            last_name,
-            user_accounts,
-            newest_enrollment,
-            small_group_mentors,
-        } = self;
-
-        // There should be at most one enrollment
-        let newest_enrollment = newest_enrollment.first();
-        // And at most one small group
-        let small_group_mentored: Option<_> = small_group_mentors
-            // Get the first small_group_mentors object
-            .first()
-            // take a reference to the inner small_group object.
-            .map(|o| &o.small_group);
-
-        DevelopersPageUser {
-            // inherit trivial fields
-            username,
-            first_name,
-            last_name,
-            user_accounts,
-            // compute remaining fields
-            is_current_coordinator: current_semester_id
-                // Map the semester if it exists.
-                .and_then(|semester_id| {
-                    newest_enrollment
-                        // Check that the enrollment if for the current semester
-                        .filter(|enrollment| enrollment.semester_id == semester_id.as_str())
-                        // Map it to the is_coordinator field
-                        .map(|enrollment| enrollment.is_coordinator)
-                })
-                // If there is no semester or an internal field is null,
-                // the user is not a coordinator.
-                .unwrap_or(false),
-
-            small_group_id: current_semester_id
-                // Map the current semester if it exists.
-                .and_then(|semester_id| {
-                    small_group_mentored
-                        // Filter for the same semester
-                        .filter(|small_group| small_group.semester_id == semester_id.as_str())
-                        // Map to the small group id
-                        .map(|small_group| small_group.small_group_id)
-                }),
-        }
-    }
-}
-
-/// Simplified structure reflecting response data from developers query.
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct DevelopersResponse {
-    /// The title of the current semester
-    pub current_semester_title: Option<String>,
-    /// The users to display on the developers page.
-    pub users: Vec<DevelopersPageUser>,
-}
-
-impl ResponseData {
-    /// Extract the current semester from the Developers query.
-    fn current_semester(&self) -> Option<&DevelopersCurrentSemester> {
-        self.current_semester.first()
-    }
-
-    /// Convert this response into a simplified
-    pub fn simplify(self) -> DevelopersResponse {
-        // Extract the current semester
-        let current_semester = self.current_semester();
-        // Extract and clone the semester title
-        let current_semester_title: Option<String> = current_semester.map(|s| s.title.clone());
-        // Extract and clone the semester id
-        let current_semester_id: Option<String> = current_semester.map(|s| s.semester_id.clone());
-
-        // Build and return a simplified response
-        DevelopersResponse {
-            current_semester_title,
-            users: self
-                .users
-                // Convert the user list to an iterator
-                .into_iter()
-                // and resolve each user's titles for the current semester.
-                .map(|u| u.resolve(current_semester_id.as_ref()))
-                // Collect into vector.
-                .collect(),
-        }
-    }
+    pub async fn get(page_num: u32, search: Option<String>, include_old: bool) ->
 }
--- src/web/services/user/developers.rs
+++ src/web/services/user/developers.rs
@@ -1,116 +1,30 @@
 //! Developers page services
 
 use crate::api::rcos::users::developers_page::DevelopersResponse;
-use crate::api::rcos::{
-    send_query,
-    users::developers_page::{
-        developers::{order_by, users_order_by},
-        Developers,
-    },
-};
+use crate::api::rcos::users::developers_page::Developers;
 use crate::error::TelescopeError;
 use crate::templates::Template;
 use crate::web::services::auth::identity::Identity;
 use actix_web::web::Query;
 
-/// The default value for the number of users per page.
-fn twenty() -> u32 {
-    20
-}
-
 /// The path to the developers page template from the templates directory.
 const TEMPLATE_PATH: &'static str = "user/developers";
 
-/// Which field should users be ordered by.
-#[derive(Copy, Clone, Debug, Serialize, Deserialize)]
-#[serde(rename_all = "snake_case")]
-pub enum OrderByField {
-    FirstName,
-    LastName,
-}
-
-impl Default for OrderByField {
-    fn default() -> Self {
-        OrderByField::FirstName
-    }
-}
-
-/// What order to use.
-#[derive(Copy, Clone, Debug, Serialize, Deserialize)]
-pub enum OrderBy {
-    #[serde(rename = "asc")]
-    Ascending,
-    #[serde(rename = "desc")]
-    Descending,
-}
-
-impl Default for OrderBy {
-    fn default() -> Self {
-        OrderBy::Ascending
-    }
-}
-
-impl Into<order_by> for OrderBy {
-    fn into(self) -> order_by {
-        match self {
-            OrderBy::Ascending => order_by::asc,
-            OrderBy::Descending => order_by::desc,
-        }
-    }
-}
-
 /// The query parameters passed to the developers page indicating pagination
 /// data and any filters.
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, Default)]
 pub struct DevelopersPageQuery {
     /// What page number to go to. Default to 0.
     #[serde(default)]
     pub page: u32,
 
-    /// How many per page. Default to 20.
-    #[serde(default = "twenty")]
-    pub per_page: u32,
-
     /// Filter for users if their first name, last name, or username contains
     /// this string case independently (via ILIKE).
     pub search: Option<String>,
 
-    /// Order the developers by a given field.
+    /// Should the results include previous members?
     #[serde(default)]
-    pub order_by: OrderByField,
-
-    /// Ascending or descending order.
-    #[serde(default)]
-    pub order: OrderBy,
-}
-
-impl Default for DevelopersPageQuery {
-    fn default() -> Self {
-        DevelopersPageQuery {
-            page: 0,
-            per_page: twenty(),
-            search: None,
-            order_by: OrderByField::default(),
-            order: OrderBy::default(),
-        }
-    }
-}
-
-impl DevelopersPageQuery {
-    /// Convert this structure's order parameters to an order_by query.
-    fn order_by_param(&self) -> users_order_by {
-        match self.order_by {
-            OrderByField::FirstName => users_order_by {
-                first_name: Some(self.order.into()),
-                ..users_order_by::default()
-            },
-
-            OrderByField::LastName => users_order_by {
-                last_name: Some(self.order.into()),
-                ..users_order_by::default()
-            },
-        }
-    }
+    pub include_old: bool,
 }
 
 /// The developer catalogue. This page displays all of the users in the
