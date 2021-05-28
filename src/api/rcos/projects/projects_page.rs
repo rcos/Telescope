06261bf84630dec2c3225baf0e435e820c961319
Nia Calia-Bogan <acaliabogan@acaliabogan.dev>
2021-05-28 19:24:05 -0400

    Project Page queries

--- src/api/rcos/projects/projects_page.rs
+++ src/api/rcos/projects/projects_page.rs
@@ -2,11 +2,20 @@
 
 use crate::api::rcos::prelude::*;
 
+/// GraphQL query to get projects with enrollments in an ongoing semester.
 #[derive(GraphQLQuery)]
 #[graphql(
     schema_path = "graphql/rcos/schema.json",
     query_path = "graphql/rcos/projects/projects.graphql",
     response_derives = "Debug,Clone,Serialize"
 )]
-pub struct Projects;
+pub struct CurrentProjects;
 
+/// GraphQL query to get all projects.
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "graphql/rcos/schema.json",
+    query_path = "graphql/rcos/projects/projects.graphql",
+    response_derives = "Debug,Clone,Serialize"
+)]
+pub struct AllProjects;
