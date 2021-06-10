ea5968ff1d7a5a04e2e86faa9f70f0de2bec1e1a
Nia Calia-Bogan <acaliabogan@acaliabogan.dev>
2021-06-10 18:15:16 -0400

    edit semester mutation

--- src/api/rcos/semesters/mutations/edit.rs
+++ src/api/rcos/semesters/mutations/edit.rs
@@ -1,6 +1,9 @@
 //! Edit mutation on semesters.
 
 use crate::api::rcos::prelude::*;
+use chrono::NaiveDate;
+use crate::error::TelescopeError;
+use crate::api::rcos::send_query;
 
 /// Type representing GraphQL mutation to make changes to a semester.
 #[derive(GraphQLQuery)]
@@ -10,3 +13,22 @@ use crate::api::rcos::prelude::*;
     response_derives = "Debug,Clone,Serialize"
 )]
 pub struct EditSemester;
+
+impl EditSemester {
+    /// Send a semester edit mutation. Return a semester ID if there was a semester found and edited.
+    pub async fn execute(
+        id: String,
+        new_title: Option<String>,
+        new_start: Option<NaiveDate>,
+        new_end: Option<NaiveDate>
+    ) -> Result<Option<String>, TelescopeError> {
+        send_query::<Self>(edit_semester::Variables {
+            semester_id: id,
+            set_title: new_title,
+            set_start: new_start,
+            set_end: new_end,
+        })
+            .await
+            .map(|data| data.update_semesters_by_pk.map(|obj| obj.semester_id))
+    }
+}
