7f5e6e93201eb9b47425d11a80096eae5ae67fc6
Nia Calia-Bogan <acaliabogan@acaliabogan.dev>
2021-06-10 17:40:12 -0400

    Refactor Semesters

--- src/web/services/admin/semesters/create.rs
+++ src/web/services/admin/semesters/create.rs
@@ -1,53 +1,15 @@
-//! Services for the semester records management page.
+//! Semester record creation.
 
-use actix_web::web::{ServiceConfig, Path, Form};
-use crate::templates::Template;
-use crate::error::TelescopeError;
-use actix_web::{web as aweb, HttpResponse};
-use crate::api::rcos::semesters::get::{
-    Semesters,
-    PER_PAGE
-};
-use crate::templates::pagination::PaginationInfo;
-use actix_web::HttpRequest;
-use chrono::NaiveDate;
-use crate::templates::forms::FormTemplate;
-use regex::Regex;
 use crate::api::rcos::semesters::mutations::create::CreateSemester;
+use crate::error::TelescopeError;
+use crate::templates::forms::FormTemplate;
+use actix_web::{
+    web::Form,
+    HttpResponse
+};
+use crate::web::services::admin::semesters::semester_id_valid;
 use actix_web::http::header::LOCATION;
-
-/// Register semester services.
-pub fn register(config: &mut ServiceConfig) {
-    config
-        .service(new)
-        .service(submit_new)
-        .service(edit)
-        .service(submit_edit)
-        .route("/semesters", aweb::get().to(index))
-        .route("/semesters/{page}", aweb::get().to(index));
-}
-
-/// Page to display previous semesters and allow edits.
-async fn index(req: HttpRequest, page_num: Option<Path<u32>>) -> Result<Template, TelescopeError> {
-    // Resolve the page number. Default to Page 1.
-    let page_num: u32 = page_num.map(|path| path.0).unwrap_or(1);
-
-    // Send the API query to get semester data.
-    let semester_data = Semesters::get(page_num - 1).await?;
-
-    // Extract the semester count if available.
-    let semester_count = semester_data.semester_count()
-        .ok_or(TelescopeError::ise("Semester count not returned by API"))?
-        as u64;
-
-    // Render template and send back to user.
-    Template::new("admin/semesters/index")
-        .field("pagination", PaginationInfo::new(semester_count, PER_PAGE as u64, page_num as u64))
-        .field("data", semester_data)
-        // Render in page
-        .render_into_page(&req, "Semester Records")
-        .await
-}
+use chrono::NaiveDate;
 
 /// Create an empty form template for semester creation.
 fn new_semester_form_empty() -> FormTemplate {
@@ -56,13 +18,13 @@ fn new_semester_form_empty() -> FormTemplate {
 
 /// Semester creation.
 #[get("/semesters/create")]
-async fn new() -> FormTemplate {
+pub async fn new() -> FormTemplate {
     new_semester_form_empty()
 }
 
 /// Form fields submitted when creating a semester record.
 #[derive(Debug, Deserialize, Serialize)]
-struct CreateSemesterForm {
+pub struct CreateSemesterForm {
     /// Semester IDs should be 6 digit strings, as used by the RPI registrar.
     id: String,
     title: String,
@@ -70,18 +32,9 @@ struct CreateSemesterForm {
     end: NaiveDate,
 }
 
-lazy_static!{
-    static ref SEMESTER_ID_REGEX: Regex = Regex::new(r"^[[:digit:]]{6}$").expect("Bad Regex");
-}
-
-/// Check if a semester ID is properly formatted (6 digit form) via regex.
-fn semester_id_valid(id: &str) -> bool {
-    SEMESTER_ID_REGEX.is_match(id)
-}
-
 /// Semester creation forms are submitted here.
 #[post("/semesters/create")]
-async fn submit_new(Form(input): Form<CreateSemesterForm>) -> Result<HttpResponse, TelescopeError> {
+pub async fn submit_new(Form(input): Form<CreateSemesterForm>) -> Result<HttpResponse, TelescopeError> {
     // Destructure form submission
     let CreateSemesterForm {id, title, start, end} = input;
 
@@ -137,12 +90,3 @@ async fn submit_new(Form(input): Form<CreateSemesterForm>) -> Result<HttpRespons
         .finish())
 }
 
-#[get("/semesters/edit/{semester_id}")]
-async fn edit(Path(semester_id): Path<String>) -> Result<FormTemplate, TelescopeError> {
-    Err(TelescopeError::NotImplemented)
-}
-
-#[get("/semesters/edit/{semester_id}")]
-async fn submit_edit(Path(semester_id): Path<String>) -> Result<HttpResponse, TelescopeError> {
-    Err(TelescopeError::NotImplemented)
-}
---
+++ src/web/services/admin/semesters/mod.rs
@@ -0,0 +1,72 @@
+//! Services for the semester records management page.
+
+mod create;
+
+use actix_web::{HttpResponse, web as aweb};
+use actix_web::http::header::LOCATION;
+use actix_web::HttpRequest;
+use actix_web::web::{Form, Path, ServiceConfig};
+use chrono::NaiveDate;
+use regex::Regex;
+
+use crate::api::rcos::semesters::get::{
+    PER_PAGE,
+    Semesters
+};
+use crate::api::rcos::semesters::mutations::create::CreateSemester;
+use crate::error::TelescopeError;
+use crate::templates::forms::FormTemplate;
+use crate::templates::pagination::PaginationInfo;
+use crate::templates::Template;
+
+/// Register semester services.
+pub fn register(config: &mut ServiceConfig) {
+    config
+        .service(create::new)
+        .service(create::submit_new)
+        .service(edit)
+        .service(submit_edit)
+        .route("/semesters", aweb::get().to(index))
+        .route("/semesters/{page}", aweb::get().to(index));
+}
+
+/// Page to display previous semesters and allow edits.
+async fn index(req: HttpRequest, page_num: Option<Path<u32>>) -> Result<Template, TelescopeError> {
+    // Resolve the page number. Default to Page 1.
+    let page_num: u32 = page_num.map(|path| path.0).unwrap_or(1);
+
+    // Send the API query to get semester data.
+    let semester_data = Semesters::get(page_num - 1).await?;
+
+    // Extract the semester count if available.
+    let semester_count = semester_data.semester_count()
+        .ok_or(TelescopeError::ise("Semester count not returned by API"))?
+        as u64;
+
+    // Render template and send back to user.
+    Template::new("admin/semesters/index")
+        .field("pagination", PaginationInfo::new(semester_count, PER_PAGE as u64, page_num as u64))
+        .field("data", semester_data)
+        // Render in page
+        .render_into_page(&req, "Semester Records")
+        .await
+}
+
+lazy_static!{
+    static ref SEMESTER_ID_REGEX: Regex = Regex::new(r"^[[:digit:]]{6}$").expect("Bad Regex");
+}
+
+/// Check if a semester ID is properly formatted (6 digit form) via regex.
+fn semester_id_valid(id: &str) -> bool {
+    SEMESTER_ID_REGEX.is_match(id)
+}
+
+#[get("/semesters/edit/{semester_id}")]
+async fn edit(Path(semester_id): Path<String>) -> Result<FormTemplate, TelescopeError> {
+    Err(TelescopeError::NotImplemented)
+}
+
+#[get("/semesters/edit/{semester_id}")]
+async fn submit_edit(Path(semester_id): Path<String>) -> Result<HttpResponse, TelescopeError> {
+    Err(TelescopeError::NotImplemented)
+}
