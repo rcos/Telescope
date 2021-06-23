615b5c6d8234890bab34e77d2e4e2c62d41d25b1
Nia Calia-Bogan <acaliabogan@acaliabogan.dev>
2021-06-22 20:30:53 -0400

    Stub new api query function, fix rustc stack overflow

--- src/api/rcos/meetings/creation/context.rs
+++ src/api/rcos/meetings/creation/context.rs
@@ -4,14 +4,14 @@ use crate::api::rcos::prelude::*;
 use crate::api::rcos::send_query;
 use crate::error::TelescopeError;
 
-/// ZST representing the GraphQL query to resolve meeting creation context.
-#[derive(GraphQLQuery)]
-#[graphql(
-    schema_path = "graphql/rcos/schema.json",
-    query_path = "graphql/rcos/meetings/creation/context.graphql",
-    response_derives = "Debug,Clone,Serialize"
-)]
-pub struct CreationContext;
+// /// ZST representing the GraphQL query to resolve meeting creation context.
+// #[derive(GraphQLQuery)]
+// #[graphql(
+//     schema_path = "graphql/rcos/schema.json",
+//     query_path = "graphql/rcos/meetings/creation/context.graphql",
+//     response_derives = "Debug,Clone,Serialize"
+// )]
+// pub struct CreationContext;
 
 // impl CreationContext {
 //     /// Get the meeting creation context.
