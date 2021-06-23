rcos/Telescope

commit fa208788827fcf566998edd73693c46712fd5f83

Compiler is currently stack overflowing
https://github.com/rust-lang/rust/issues/86556
https://github.com/graphql-rust/graphql-client/issues/370

src/api/rcos/meetings/creation/context.rs
@@ -13,44 +13,14 @@ use crate::error::TelescopeError;
 )]
 pub struct CreationContext;
 
-use creation_context::String_comparison_exp as Filter;
-
-impl Default for Filter {
-    fn default() -> Self {
-        Self {
-            eq: None,
-            gt: None,
-            gte: None,
-            ilike: None,
-            in_: None,
-            is_null: None,
-            like: None,
-            lt: None,
-            lte: None,
-            neq: None,
-            nilike: None,
-            nin: None,
-            nlike: None,
-            nsimilar: None,
-            similar: None,
-        }
-    }
-}
-
-impl CreationContext {
-    /// Get the meeting creation context.
-    pub async fn get(host: Option<String>) -> Result<creation_context::ResponseData, TelescopeError> {
-        // Convert host into filter to send in query.
-        let filter = host
-            .clone()
-            .map(|username| Filter { eq: Some(username), ..Default::default() })
-            .unwrap_or(Filter { is_null: Some(true), ..Default::default() });
-
-        send_query::<Self>(creation_context::Variables {
-            now: chrono::Utc::today().naive_utc(),
-            host_filter: filter,
-            host_username: host
-        })
-        .await
-    }
-}
+// impl CreationContext {
+//     /// Get the meeting creation context.
+//     pub async fn get(host: Option<String>) -> Result<creation_context::ResponseData, TelescopeError> {
+//
+//         send_query::<Self>(creation_context::Variables {
+//             now: chrono::Utc::today().naive_utc(),
+//             host_username: host.map(||)
+//         })
+//         .await
+//     }
+// }

src/web/services/meetings/create.rs
@@ -154,5 +154,6 @@ async fn submit_meeting(query: Option<Query<FinishQuery>>, Form(form): Form<Fini
     let host: Option<String> = query.map(|q| q.host.clone());
 
 
+
     Err(TelescopeError::NotImplemented)
 }

graphql/rcos/meetings/creation/context.graphql
@@ -1,16 +1,8 @@
-query CreationContext($now: date!, $host_filter: String_comparison_exp!, $host_username: String) {
+query CreationContext($now: date!, $host_username: [String!]!, $semester_filter: semesters_bool_exp!) {
     # Semesters in which a meeting can be created. This will be
     # all current and duture semesters in ascending order.
     available_semesters: semesters(
-        where: {
-            end_date: {_gte: $now},
-
-            # Require that the semester is one the host is enrolled in.
-            # This has the side effect of requireing that there is at least
-            # one enrollment for a semester
-            enrollments: {username: {_eq: $host_username}},
-
-        },
+        where: $semester_filter,
         order_by: {start_date: asc}
     ) {
         semester_id
@@ -21,7 +13,7 @@ query CreationContext($now: date!, $host_filter: String_comparison_exp!, $host_u
 
     # We get around the possibity of no host by letting the rust side make the filter.
     # Setting _is_null to true when there's no host, or specifying otherwise.
-    host: users(where: {username: $host_filter}, limit: 1) {
+    host: users(where: {username: {_in: $host_username}}, limit: 1) {
         username
         first_name
         last_name

commit 5e86a0af1cd36142b9ed1b7a432db20edf4cfcb8

Working build
fixes issues introduced in fa208788827fcf566998edd73693c46712fd5f83

src/api/rcos/meetings/creation/context.rs
@@ -1,26 +1,34 @@
 //! GraphQL query to get context for meeting creation.
 
 use crate::api::rcos::prelude::*;
-use crate::api::rcos::send_query;
+use crate::api::rcos::send_json_query;
 use crate::error::TelescopeError;
+use serde_json::Value;
+use chrono::Utc;
 
-// /// ZST representing the GraphQL query to resolve meeting creation context.
-// #[derive(GraphQLQuery)]
-// #[graphql(
-//     schema_path = "graphql/rcos/schema.json",
-//     query_path = "graphql/rcos/meetings/creation/context.graphql",
-//     response_derives = "Debug,Clone,Serialize"
-// )]
-// pub struct CreationContext;
+/// The GraphQL query.
+const QUERY_STRING: &'static str = include_str!("../../../../../graphql/rcos/meetings/creation/context.graphql");
 
-// impl CreationContext {
-//     /// Get the meeting creation context.
-//     pub async fn get(host: Option<String>) -> Result<creation_context::ResponseData, TelescopeError> {
-//
-//         send_query::<Self>(creation_context::Variables {
-//             now: chrono::Utc::today().naive_utc(),
-//             host_username: host.map(||)
-//         })
-//         .await
-//     }
-// }
+/// Get the meeting creation context. This is done in JSON format because the typed version was
+/// causing the compiler to stack overflow.
+pub async fn get_context(host_username: Option<String>) -> Result<Value, TelescopeError> {
+    // Make query variables.
+    let mut variables: Value = json!({
+        // Use an empty or single item list as a work around for non-nullable types.
+        "host_username": host_username.clone().map(|username| vec![username]).unwrap_or(Vec::new()),
+        "semester_filter": {
+            "end_date": {
+                "_gte": Utc::today().naive_utc()
+            }
+        }
+    });
+
+    // Add the extra clause to the filter variable if there is a host.
+    // This will filter to semesters the host is enrolled in.
+    if let Some(username) = host_username {
+        variables["semester_filter"]["enrollments"]["username"]["_eq"] = json!(username);
+    }
+
+    // Send the query and await the result.
+    send_json_query("CreationContext", QUERY_STRING, variables).await
+}

src/api/rcos/mod.rs
@@ -20,7 +20,7 @@ pub mod users;
 /// The name of this API in error messages.
 const API_NAME: &'static str = "RCOS Central Hasura GraphQL API";
 
-/// Send a GraphQL query to the central RCOS API for a given subject (or anonymously).
+/// Send a GraphQL query to the central RCOS API.
 pub async fn send_query<T: GraphQLQuery>(
     variables: T::Variables,
 ) -> Result<T::ResponseData, TelescopeError> {
@@ -48,7 +48,9 @@ pub async fn send_query<T: GraphQLQuery>(
 
 
 /// Send an API query using the GraphQL JSON format. This is useful for avoiding issues in the
-/// macro-generated GraphQL types
+/// macro-generated GraphQL types.
+///
+/// The typed version should generally be used instead to avoid runtime type errors.
 pub async fn send_json_query(query_name: &str, query_document: &str, variables: Value) -> Result<Value, TelescopeError> {
     // Build the GraphQL request body.
     let request_body: Value = json!({

src/web/services/meetings/create.rs
@@ -15,12 +15,13 @@ use actix_web::web as aweb;
 use crate::templates::Template;
 use crate::api::rcos::meetings::creation::host_selection::HostSelection;
 use actix_web::HttpRequest;
-use crate::api::rcos::meetings::creation::context::CreationContext;
+use crate::api::rcos::meetings::creation;
 use crate::api::rcos::meetings::{ALL_MEETING_TYPES, MeetingType};
 use actix_web::HttpResponse;
 use chrono::{NaiveDate, NaiveTime};
 use url::Url;
 use std::collections::HashMap;
+use serde_json::Value;
 
 /// Authorization function for meeting creation.
 fn meeting_creation_authorization(username: String) -> LocalBoxFuture<'static, AuthorizationResult> {
@@ -86,8 +87,10 @@ fn finish_form() -> FormTemplate {
 async fn finish(query: Option<Query<FinishQuery>>) -> Result<FormTemplate, TelescopeError> {
     // Extract query parameter.
     let host: Option<String> = query.map(|q| q.host.clone());
+
     // Query RCOS API for meeting creation context.
-    let context = CreationContext::get(host).await?;
+    let context: Value = creation::context::get_context(host).await?;
+
     // Create the form template to finish meeting creation.
     let mut form: FormTemplate = finish_form();
 