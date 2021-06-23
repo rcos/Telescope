5c196a0b41b4c070c50cff2daedc50d799ce29d2
Nia Calia-Bogan <acaliabogan@acaliabogan.dev>
2021-06-23 17:30:24 -0400

    tweak mutation

--- src/api/rcos/meetings/creation/create.rs
+++ src/api/rcos/meetings/creation/create.rs
@@ -1,13 +1,50 @@
 //! GraphQL mutation to create a meeting.
 
 use crate::api::rcos::prelude::*;
+use crate::error::TelescopeError;
+use crate::api::rcos::send_query;
+use chrono::{DateTime, Utc};
+use crate::api::rcos::meetings::MeetingType;
 
 #[derive(GraphQLQuery)]
 #[graphql(
     schema_path = "graphql/rcos/schema.json",
     query_path = "graphql/rcos/meetings/creation/create.graphql",
-    response_derives = "Debug,Clone,Serialize"
+    response_derives = "Debug,Copy,Clone,Serialize"
 )]
 pub struct CreateMeeting;
 
-impl CreateMeeting {}
+impl CreateMeeting {
+    /// Execute a meeting creation mutation. Return the created meeting's ID.
+    pub async fn execute(
+        host_username: Option<String>,
+        title: Option<String>,
+        start: DateTime<Utc>,
+        end: DateTime<Utc>,
+        description: String,
+        is_draft: bool,
+        is_remote: bool,
+        location: Option<String>,
+        meeting_url: Option<String>,
+        recording_url: Option<String>,
+        external_slides_url: Option<String>,
+        semester_id: String,
+        kind: MeetingType
+    ) -> Result<Option<i64>, TelescopeError> {
+        send_query::<Self>(create_meeting::Variables {
+            host_username,
+            title,
+            start,
+            end,
+            description,
+            is_draft,
+            is_remote,
+            location,
+            meeting_url,
+            recording_url,
+            external_slides_url,
+            semester_id,
+            kind
+        }).await.map(|response| response.insert_meetings_one.map(|obj| obj.meeting_id))
+    }
+}
