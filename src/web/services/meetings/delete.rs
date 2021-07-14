5513a48c751f70c0e8fa548ba35342ea2e3f843d
Nia Calia-Bogan <acaliabogan@acaliabogan.dev>
2021-07-14 15:34:15 -0400

    Stub meeting deletion

--- src/web/services/meetings/delete.rs
+++ src/web/services/meetings/delete.rs
@@ -1,6 +1,18 @@
 //! Services for deleting meetings.
 
-use actix_web::web::ServiceConfig;
+use actix_web::web::{ServiceConfig, Path};
+use crate::web::services::auth::identity::AuthenticationCookie;
+use actix_web::HttpResponse;
+use crate::error::TelescopeError;
 
 /// Register meeting deletion services.
-pub fn register(config: &mut ServiceConfig) {}
+pub fn register(config: &mut ServiceConfig) {
+    config
+        .service(delete_meeting);
+}
+
+
+#[get("/meeting/{meeting_id}/delete")]
+async fn delete_meeting(auth: AuthenticationCookie, Path( meeting_id ): Path<i64>) -> Result<HttpResponse, TelescopeError> {
+    return Err(TelescopeError::NotImplemented);
+}
