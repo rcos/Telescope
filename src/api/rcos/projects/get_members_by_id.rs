4e32b48a91a11bc6719ed644c164fca32e365a91
yemily17 <46946140+yemily17@users.noreply.github.com>
2021-08-21 16:26:49 -0400

    project info

---
+++ src/api/rcos/projects/get_members_by_id.rs
@@ -0,0 +1,30 @@
+// ! GraphQL query to get the username of the host of a meeting by the meeting's ID.
+
+
+use crate::error::TelescopeError;
+use crate::api::rcos::send_query;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "graphql/rcos/schema.json",
+    query_path = "graphql/rcos/projects/get_members_by_id.graphql",
+    response_derives = "Debug,Clone,Serialize"
+)]
+pub struct Members;
+
+use self::members::{MembersMembers, Variables};
+
+impl Members {
+    /// Get the username of the host of a meeting if there is one.
+    pub async fn get(
+        id: i64,
+    ) -> Result<Vec<MembersMembers>, TelescopeError> {
+        Ok(send_query::<Self>(Variables {
+            id,
+        })
+        .await?
+        .members
+        )
+    }
+}
+
--- src/api/rcos/projects/mod.rs
+++ src/api/rcos/projects/mod.rs
@@ -1,3 +1,4 @@
 //! RCOS API interactions related to projects.
 
+pub mod get_members_by_id;
 pub mod projects_page;
