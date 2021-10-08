a9b1d23e8875d0843f4c42c271706d2f2eda6cfc
Nia Calia-Bogan <acaliabogan@acaliabogan.dev>
2021-10-08 17:31:18 -0400

    Update discord branding on auth template

--- src/templates/auth.rs
+++ src/templates/auth.rs
@@ -69,7 +69,7 @@ pub fn login() -> Template {
         item(
             DiscordOAuth::login_path(),
             "btn-discord mb-2",
-            "Login using Discord",
+            "Login using",
             // This is manually coded for in the template file and is not
             // a Feather icon. Do not use it in other places, as it won't work.
             Some("discord"),
