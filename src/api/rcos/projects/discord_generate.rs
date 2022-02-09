0a02c1f7c39e1c3e966bbf99d1bf62e0637dbac1
ArberSephirotheca <sephirotheca17@gmail.com>
2022-02-09 01:24:45 -0500

    src/discord_bot/commands: generate.rs, mod.rs
    Created Discord embed for generate command.

--- src/api/rcos/projects/discord_generate.rs
+++ src/api/rcos/projects/discord_generate.rs
@@ -1,12 +1,22 @@
-
+/*
 use crate::api::rcos::prelude::*;
 use crate::api::rcos::send_query;
+use crate::api::rcos::projects::projects_page::{AllProjects, CurrentProjects};
 use crate::error::TelescopeError;
 
 /// ZST representing the associated GraphQL query.
 #[derive(GraphQLQuery)]
 #[graphql(
     schema_path = "graphql/rcos/schema.json",
-    query_path = "graphql/rcos/users/discord_whois.graphql",
+    query_path = "graphql/rcos/users/discord_generate.graphql",
     response_derives = "Debug,Clone,Serialize"
 )]
+
+pub struct DiscordGenerate;
+
+impl DiscordGenerate{
+    pub fn get_project() -> Result<ResponseData, TelescopeError>{
+
+    }
+}
+*/
--- src/discord_bot/commands/generate.rs
+++ src/discord_bot/commands/generate.rs
@@ -5,52 +5,61 @@
 // TODO: Limited to certain role
 // TODO: handle event
 
-use crate::api::rcos::users::discord_generate::DiscordGenerate;
+//use crate::api::rcos::projects::discord_generate::DiscordGenerate;
 use crate::discord_bot::commands::InteractionResult;
 use crate::env::global_config;
-use serenity::builder::{CreateApplicationCommand, CreateApplicationCommandOption,
-    CreateApplicationCommandPermissions, CreateApplicationCommandPermissionData, CreateEmbed};
+use serenity::builder::{CreateApplicationCommand, CreateApplicationCommandOption, CreateEmbed};
 use serenity::client::Context;
 use serenity::model::interactions::application_command::ApplicationCommandInteraction;
 use serenity::model::interactions::{
-    application_command::ApplicationCommandOptionType, application_command::CreateApplicationCommandPermissionType
-    ,InteractionResponseType,
+    application_command::ApplicationCommandOptionType, InteractionResponseType,
 };
 use serenity::model::prelude::InteractionApplicationCommandCallbackDataFlags;
+use serenity::model::id::{GuildId, RoleId};
 use serenity::utils::Color;
-use serenity::Result as SerenityResult;·
+use serenity::Result as SerenityResult;
 
 // The name of this slash command.
 pub const COMMAND_NAME: &'static str = "generate";
 pub const OPTION_NAME: [&'static str; 4] = ["channels", "roles", "categories", "all"];
-pub const ROLE: [&'static str; 2] = ["Faculty Advisors", "Coordinators"]
+pub const ROLE_ID: [&'static RoleId; 2] = [&RoleId(939836159134158858), &RoleId(940751879493799936)];
 pub const ERROR_COLOR: Color = Color::new(0xE6770B);
 
-// Build the option for the /generate command.
-pub fn generate_option(obj: &mut CreateApplicationCommandOption, option: &'static str) -> &mut CreateApplicationCommandOption{
-    match option{
-        OPTION_NAME[0] => {
-            obj.name(option)
-                .kind(ApplicationCommandOptionType::SubCommand)
-                .description("generate channels for projects and/or small groups.");
-        },
-        OPTION_NAME[1] => {
-            obj.name(option)
-                .kind(ApplicationCommandOptionType::SubCommand)
-                .description("generate roles for projects and/or small groups.");
-        },
-        OPTION_NAME[2] => {
-            obj.name(option)
-                .kind(ApplicationCommandOptionType::SubCommand)
-                .description("generate categories for projects and/or small groups.");
-        },
-        OPTION_NAME[3] => {
-            obj.name(option)
-                .kind(ApplicationCommandOptionType::SubCommand)
-                .description("generate all for projects and/or small groups.");
-        },
+
+pub fn has_permission(role: & RoleId) -> bool{
+    if ROLE_ID.contains(&role){
+        return true;
     }
-    return obj;
+    false
+}
+
+// Build the option for the /generate command.
+pub fn generate_option<'a>(obj: &'a mut CreateApplicationCommandOption, option: &'a str ) -> &'a mut CreateApplicationCommandOption{
+    match option{
+        _ if option == OPTION_NAME[0] => {
+            obj.name(option)
+                .kind(ApplicationCommandOptionType::SubCommand)
+                .description("generate channels for projects and/or small groups.")
+        },
+        _ if option == OPTION_NAME[1] => {
+            obj.name(option)
+                .kind(ApplicationCommandOptionType::SubCommand)
+                .description("generate roles for projects and/or small groups.")
+        },
+        _ if option == OPTION_NAME[2] => {
+            obj.name(option)
+                .kind(ApplicationCommandOptionType::SubCommand)
+                .description("generate categories for projects and/or small groups.")
+        },
+        _ if option == OPTION_NAME[3] => {
+            obj.name(option)
+                .kind(ApplicationCommandOptionType::SubCommand)
+                .description("generate all for projects and/or small groups.")
+        },
+        _ =>{
+            obj
+        }
+    }
 }
 
 /// Modify a builder object to add the info for the /generate command.
@@ -60,8 +69,7 @@ pub fn create_generate(obj: &mut CreateApplicationCommand) -> &mut CreateApplica
         .description("Generate channels, roles, categories or all. Limited to faculty advisor, coordinator, and sysamin.");
     for option in OPTION_NAME{
         obj
-        .create_option(generate_option(option))
-        .default_permission(false);
+        .create_option(|object|generate_option(object, option));
     }
     return obj;
 }
@@ -85,6 +93,66 @@ async fn handle(ctx: &Context, interaction: &ApplicationCommandInteraction) -> S
             .unwrap()
             .name
             .as_str();
-            
+        
+        // Extract the roles of invoker.
+        let roles = interaction
+            .member
+            .as_ref()
+            .unwrap()
+            .roles
+            .as_slice();
+        
+        // If missing correct options, or invoker's role does not have permission,
+        // respond with an embed indicating an error.
+        if !OPTION_NAME.contains(&option_name) ||
+            !roles.iter().any(|e| has_permission(e)){
+                return interaction.create_interaction_response(&ctx.http, |create_response|{
+                    create_response.kind(InteractionResponseType::ChannelMessageWithSource)
+                        .interaction_response_data(|rdata| {
+                            rdata
+                             // Do not allow any mentions
+                            .allowed_mentions(|am| am.empty_parse())
+                             // Use the ephemeral flag to mark the response as only visible to the user who invoked it.
+                            .flags(InteractionApplicationCommandCallbackDataFlags::EPHEMERAL)                            
+                            .create_embed(|embed| {
+                                // Add common attributes
+                                embed_common(embed)
+                                    .color(ERROR_COLOR)
+                                    .title("Permission Error")
+                                    .description(
+                                        "We could not generate channels/roles/categories for you due to lacking of permission."
+                                    )
+                                    // Include the error as a field of the embed.
+                                    .field("Error Message","Permissoion Error", false)
+                            })
+                        })
+                }).await;
+            }else{
+                return interaction.create_interaction_response(&ctx.http, |create_response|{
+                    create_response.kind(InteractionResponseType::ChannelMessageWithSource)
+                        .interaction_response_data(|rdata| {
+                            rdata.content("ok")
+                        })
+                }).await;
+            }
+    
+}
 
+
+/// Add common data to a Discord embed. This includes the author, footer, and timestamp.
+fn embed_common(create_embed: &mut CreateEmbed) -> &mut CreateEmbed {
+    create_embed
+        // Timestamp is always now
+        .timestamp(&chrono::Utc::now())
+        // Footer is telescope version
+        .footer(|create_footer| {
+            create_footer.text(format!("Telescope {}", env!("CARGO_PKG_VERSION")))
+        })
+        // Author links to telescope's github.
+        .author(|create_author| {
+            create_author
+                // Don't include the telescope icon - we only link to the github
+                .name("Telescope")
+                .url("https://github.com/rcos/Telescope")
+        })
 }
