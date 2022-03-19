e9d53e21d8dccef309c8af727dcd8e7b850fb94c
bep <8226605+bepvte@users.noreply.github.com>
2022-03-18 20:11:24 -0400

    Initial projects page

--- src/web/services/projects/projects_page.rs
+++ src/web/services/projects/projects_page.rs
@@ -1,9 +1,16 @@
 //! Project page.
-
+use crate::api::rcos::projects::projects_page;
 use crate::error::TelescopeError;
-use actix_web::HttpResponse;
+use crate::templates::page::Page;
+use crate::templates::Template;
+use actix_web::HttpRequest;
+
+const TEMPLATE_PATH: &'static str = "projects/list";
 
 #[get("/projects")]
-pub async fn get() -> Result<HttpResponse, TelescopeError> {
-    return Err(TelescopeError::NotImplemented);
+pub async fn get(req: HttpRequest) -> Result<Page, TelescopeError> {
+    let projects = projects_page::CurrentProjects::get(0, None).await?;
+    let mut template = Template::new(TEMPLATE_PATH);
+    template.fields = json!({ "projects": projects.projects });
+    return template.in_page(&req, "RCOS Projects").await;
 }
