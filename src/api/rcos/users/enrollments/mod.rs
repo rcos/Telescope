5df92406d00075e728bb0c2383250fac7e1753eb
Wyatt Ross <2treecko@gmail.com>
2022-10-21 19:45:11 -0400

    Editing enrollments works. It's kinda jank, and I'd like to add a proper landing page, but it works.

--- src/api/rcos/users/enrollments/mod.rs
+++ src/api/rcos/users/enrollments/mod.rs
@@ -3,3 +3,4 @@
 pub mod enrollments_lookup;
 pub mod user_enrollment_lookup;
 pub mod enrollment_by_ids;
+pub mod edit_enrollment;
--- src/web/services/coordinate/enrollments/edit.rs
+++ src/web/services/coordinate/enrollments/edit.rs
@@ -1,35 +1,29 @@
 use crate::error::TelescopeError;
 use crate::templates::page::Page;
-use crate::api::rcos::users::enrollments::{
-    enrollment_by_ids::{EnrollmentByIds},
-};
-use crate::api::rcos::projects::projects_page::CurrentProjects;
+use crate::api::rcos::users::enrollments::enrollment_by_ids::EnrollmentByIds;
 use crate::templates::Template;
 use actix_web::web::Form;
-use actix_web::http::header::LOCATION;
-use chrono::{DateTime, Local, NaiveDateTime, NaiveTime, TimeZone, Utc};
-use serde_json::Value;
-use actix_web::web::{self as aweb, Path, Query, ServiceConfig, HttpRequest};
+use crate::api::rcos::users::enrollments::edit_enrollment::edit_enrollment::Variables;
+use crate::api::rcos::users::enrollments::edit_enrollment;
+use actix_web::web::{Path, ServiceConfig, HttpRequest};
 use crate::api::rcos::prelude::*;
 
 const ENROLLMENT_EDIT_FORM: &str = "coordinate/enrollments/edit/form";
 
 pub fn register(config: &mut ServiceConfig){
-    config.route(
-        "/semesters/enrollments/{semester_id}/{user_id}/edit",
-        aweb::get().to(edit_page),
-                 );
+    config
+        .service(edit_page)
+        .service(submit_edits);
 }
 
+#[get("/semesters/enrollments/{semester_id}/{user_id}/edit")]
 pub async fn edit_page(
 req: HttpRequest,
 Path((semester_id, user_id)): Path<(String, String)>,
 ) -> Result<Page, TelescopeError>{
-    let uid = user_id.parse::<uuid>().ok().unwrap();
-    let enrollment_data =  EnrollmentByIds::get(uid, semester_id).await?;
+    let uuid = user_id.parse::<uuid>().ok().unwrap();
+    let enrollment_data =  EnrollmentByIds::get(uuid, semester_id).await?;
    
-    dbg!("{}", &enrollment_data);
-
     let mut form = Template::new(ENROLLMENT_EDIT_FORM);
     form.fields = json!({
         "data": enrollment_data,
@@ -39,6 +33,73 @@ Path((semester_id, user_id)): Path<(String, String)>,
     form.in_page(&req, "Edit Enrollment").await
 }
 
-pub async fn submit_edits(){
+#[post("/semesters/enrollments/{semester_id}/{user_id}/edit")]
+pub async fn submit_edits(
+req: HttpRequest,
+Path((semester_id, user_id)): Path<(String, String)>,
+Form(form_data): Form<EnrollmentForm>,
+) -> Result<Page, TelescopeError>{
+    let uid = user_id.parse::<uuid>().ok().unwrap();
 
+    let EnrollmentForm {
+        lead,
+        coordinator,
+        pay,
+        project,
+        credit,
+        mid_grade,
+        final_grade,
+    } = form_data;
+    let uuid = user_id.parse::<uuid>().ok().unwrap();
+
+    let edit_variables = Variables {
+        semester_id: semester_id.clone(),
+        user_id: uuid,
+        lead,
+        coordinator,
+        pay,
+        project: project.unwrap(),
+        credits: credit.unwrap(),
+        mid_grade: mid_grade.unwrap(),
+        final_grade: final_grade.unwrap(),
+    };
+
+    let _user_id = edit_enrollment::EditEnrollment::execute(edit_variables).await;
+
+    //We make another request here immediately after editing it, maybe not ideal speed wise.
+    //Consider changing later?
+    let enrollment_data =  EnrollmentByIds::get(uid, semester_id).await?;
+   
+    dbg!("{}", &enrollment_data);
+
+    let mut form = Template::new(ENROLLMENT_EDIT_FORM);
+    form.fields = json!({
+        "data": enrollment_data,
+    });
+
+    form.in_page(&req, "Edit Enrollment").await
+
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct EnrollmentForm { 
+    #[serde(default)]
+    pub lead: bool,
+
+    #[serde(default)]
+    pub coordinator: bool,
+
+    #[serde(default)]
+    pub pay: bool,
+
+    #[serde(default)]
+    pub mid_grade: Option<f64>,
+
+    #[serde(default)]
+    pub final_grade: Option<f64>,
+
+    #[serde(default)]
+    pub credit: Option<i64>,
+
+    pub project: Option<i64>, 
 }
